791e25ac2dca304661fc3f419717b0258de15cef
ismael <spacingismael@gmail.com>
2024-03-30 00:48:03 +0000

    fixing task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,16 +8,21 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left && tree->right)
 	{
-		if (tree->left->left || tree->left->right
-			|| tree->right->right || tree->right->left)
+		left = binary_tree_is_full(tree->left);
+		right = binary_tree_is_full(tree->right);
+		if (left == 0 || right == 0)
 			return (0);
+		return (1);
 	}
+	else if (!tree->left && !tree->right)
+		return (1);
 	else
 		return (0);
-	return (1);
 }
